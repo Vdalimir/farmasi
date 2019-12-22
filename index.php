dc191e7aef9c54a08b373a380b4e8f59dd5a513e
Vdalimir <vladimir.hrinin@gmail.com>
2019-12-22 11:58:14 +0200

    1

--- index.php
+++ index.php
@@ -159,7 +159,6 @@
     <div class="container text-center">
         <h1>АКЦИЯ</h1>
         <p class="text-red mt-10">красивый старт</p>
-        <p>asas</p>
     </div>
 </section>
 <script src="js/jquery-3.4.1.min.js"></script>
