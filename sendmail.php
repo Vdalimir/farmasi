3ff53022d13ad7f25ed826aedb289a5433995e64
Vdalimir <vladimir.hrinin@gmail.com>
2019-12-25 14:13:25 +0200

    pre-last modify design

--- sendmail.php
+++ sendmail.php
@@ -4,7 +4,7 @@ if(isset($_POST['first_name'])){
     $userMail = $_POST['user_mail'];
     $userPhone = $_POST['user_phone'];
 
-    $mailTo = "info@ua-farmasi.com, ofisfarmasikariuk@gmail.com";
+    $mailTo = "info@ua-farmasi.com, ofisfarmasikariuk@gmail.com, farmasi_chumak@ukr.net";
     $subject = "Заявка на консультацию";
     $message = $firstName . " отправил заявку на регистрацию.\r\nМой номер - ".$userPhone." , \r\nи моя почта - ".$userMail;
     $headers  = 'MIME-Version: 1.0' . "\r\n";
