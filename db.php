8286b9d1331c554fde1dc60e615622b77e4e6b9e
Vdalimir <vladimir.hrinin@gmail.com>
2019-12-26 14:21:01 +0200

    pre-last modify design

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
+/*define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'farmasi');
-/*define('DB_USERNAME', 'u528030755_farmasi');
+define('DB_NAME', 'farmasi');*/
+define('DB_USERNAME', 'u528030755_farmasi');
 define('DB_PASSWORD', 'Farmasi5264');
-define('DB_NAME', 'u528030755_farmasi');*/
+define('DB_NAME', 'u528030755_farmasi');
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 $link->set_charset("utf8");
 if($link === false){
