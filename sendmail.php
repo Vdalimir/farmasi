4eb3301fac1a774c3c95abed4d4bae1a614ac212
Vdalimir <vladimir.hrinin@gmail.com>
2020-02-02 17:56:56 +0200

    add telegram bot

--- sendmail.php
+++ sendmail.php
@@ -56,6 +56,6 @@ if (isset($_POST['first_name'])) {
 Имя - <strong>$firstName</strong>
 Город - <strong>$userCity</strong>
 Мобильный <a href='tel:$userPhone'>$userPhone</a>
-</pre>");
+</pre>", "qweqwe");
 
 }
