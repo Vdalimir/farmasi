cf23c037d37fcc1a98f30dd5e25abb6aeb845e0e
Vdalimir <vladimir.hrinin@gmail.com>
2020-02-02 17:49:13 +0200

    update form registration add privacy policy page

--- sendmail.php
+++ sendmail.php
@@ -8,53 +8,55 @@ try {
     $bot = new \TelegramBot\Api\Client($token);
 
 
-     $bot->command('start', function ($message) use ($bot) {
-         $answer = 'Привет! твой чат айди - '.$message->getChat()->getId();
-         $bot->sendMessage($message->getChat()->getId(), $answer);
-     });
+    $bot->command('start', function ($message) use ($bot) {
+        $answer = 'Привет! твой чат айди - ' . $message->getChat()->getId();
+        $bot->sendMessage($message->getChat()->getId(), $answer);
+    });
 
 // команда для помощи
-     $bot->command('help', function ($message) use ($bot) {
-         $answer = 'Команды:
+    $bot->command('help', function ($message) use ($bot) {
+        $answer = 'Команды:
      /help - вывод справки';
-         $bot->sendMessage($message->getChat()->getId(), $answer);
-     });
+        $bot->sendMessage($message->getChat()->getId(), $answer);
+    });
 
-     $bot->on(function ($Update) use ($bot) {
-         $message = $Update->getMessage();
-         $msg_text = $message->getText();
+    $bot->on(function ($Update) use ($bot) {
+        $message = $Update->getMessage();
+        $msg_text = $message->getText();
 
-         $bot->sendMessage($message->getChat()->getId(), "Я не понимаю ...");
-     }, function () {
-         return true;
-     });
+        $bot->sendMessage($message->getChat()->getId(), "Я не понимаю ...");
+    }, function () {
+        return true;
+    });
 
     $bot->run();
 
 } catch (\TelegramBot\Api\Exception $e) {
     $e->getMessage();
 }
-if(isset($_POST['first_name'])){
+if (isset($_POST['first_name'])) {
     $firstName = $_POST['first_name'];
     $userCity = $_POST['user_city'];
     $userPhone = $_POST['user_phone'];
 
     $mailTo = "info@ua-farmasi.com, ofisfarmasikariuk@gmail.com, farmasi_chumak@ukr.net";
     $subject = "Заявка на консультацию";
-    $message = $firstName . " отправил(а) заявку на регистрацию.\r\nМой номер - ".$userPhone." , Город - ".$userCity;
-    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $message = $firstName . " отправил(а) заявку на регистрацию.\r\nМой номер - " . $userPhone . " , Город - " . $userCity;
+    $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
     $headers .= 'From: UA-FARMASI <info@ua-farmasi.com>';
 
-    if(mail($mailTo,$subject,$message, $headers)){
+    if (mail($mailTo, $subject, $message, $headers)) {
         echo true;
-    }else {
+    } else {
         echo false;
     }
 
-    $bot->sendMEssage(450790032, "Новая заявка с сайта.
-    от - $firstName.
-    Город - $userCity.
-    Мобильный  $userPhone");
+    $bot->sendMEssage(450790032, null,
+        "<pre><b>Новая заявка с сайта</b>
+Имя - <strong>$firstName</strong>
+Город - <strong>$userCity</strong>
+Мобильный <a href='tel:$userPhone'>$userPhone</a>
+</pre>");
 
 }
