432e524c55c636a64a5e0ab97386bb2975651c52
Vdalimir <vladimir.hrinin@gmail.com>
2020-02-02 17:21:40 +0200

    update form registration add privacy policy page

--- sendmail.php
+++ sendmail.php
@@ -4,35 +4,7 @@
 $token = "882116321:AAGX1TWys8Ka9Rlh9WhdAio1QA9v_2yvSVU";
 require_once "vendor/autoload.php";
 
-try {
-    $bot = new \TelegramBot\Api\Client($token);
 
-    $bot->command('start', function ($message) use ($bot) {
-        $answer = 'Привет! Я буду отправлять тебе сообщения с сайта твой чат айди - '.$message->getChat()->getId();
-        $bot->sendMessage($message->getChat()->getId(), $answer);
-    });
-
-// команда для помощи
-    $bot->command('help', function ($message) use ($bot) {
-        $answer = 'Команды:
-        /help - вывод справки';
-        $bot->sendMessage($message->getChat()->getId(), $answer);
-    });
-
-    $bot->on(function ($Update) use ($bot) {
-        $message = $Update->getMessage();
-        $msg_text = $message->getText();
-
-        $bot->sendMessage($message->getChat()->getId(), "Я не понимаю ...");
-    }, function () {
-        return true;
-    });
-
-    $bot->run();
-
-} catch (\TelegramBot\Api\Exception $e) {
-    $e->getMessage();
-}
 if(isset($_POST['first_name'])){
     $firstName = $_POST['first_name'];
     $userCity = $_POST['user_city'];
@@ -51,5 +23,34 @@ if(isset($_POST['first_name'])){
         echo false;
     }
 
+    try {
+        $bot = new \TelegramBot\Api\Client($token);
+
+        $bot->command('start', function ($message) use ($bot) {
+            $answer = 'Привет! Я буду отправлять тебе сообщения с сайта твой чат айди - '.$message->getChat()->getId();
+            $bot->sendMessage($message->getChat()->getId(), $answer);
+        });
+
+// команда для помощи
+        $bot->command('help', function ($message) use ($bot) {
+            $answer = 'Команды:
+        /help - вывод справки';
+            $bot->sendMessage($message->getChat()->getId(), $answer);
+        });
+
+        $bot->on(function ($Update) use ($bot) {
+            $message = $Update->getMessage();
+            $msg_text = $message->getText();
+
+            $bot->sendMessage($message->getChat()->getId(), "Я не понимаю ...");
+        }, function () {
+            return true;
+        });
+
+        $bot->run();
+
+    } catch (\TelegramBot\Api\Exception $e) {
+        $e->getMessage();
+    }
 
 }
