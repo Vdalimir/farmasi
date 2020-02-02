a3399c1aeb915743b96047b240030c50c6115025
Vdalimir <vladimir.hrinin@gmail.com>
2020-02-02 17:10:35 +0200

    update form registration add privacy policy page

--- sendmail.php
+++ sendmail.php
@@ -4,6 +4,50 @@
 $token = "969745652:AAEjjUjSNmfFGNCHfa-WAlfMqKolEBYxFko";
 require_once "vendor/autoload.php";
 
+try {
+    $bot = new \TelegramBot\Api\Client($token);
+
+    $bot->command('start', function ($message) use ($bot) {
+        $answer = 'Привет! Я буду отправлять тебе сообщения с сайта твой чат айди - '.$message->getChat()->getId();
+        $bot->sendMessage($message->getChat()->getId(), $answer);
+    });
+
+// команда для помощи
+    $bot->command('help', function ($message) use ($bot) {
+        $answer = 'Команды:
+        /help - вывод справки';
+        $bot->sendMessage($message->getChat()->getId(), $answer);
+    });
+    /*$bot->command('bday', function ($message) use ($bot) {
+        $text = $message->getText();
+        $param = str_replace('/bday', '', $text);
+        if (empty($param)) {
+            $answer = 'Вы ничего не написали. Что бы отправить поздравление в начале текста введите команду /bday затем текст.
+        Пример "/bday с др"';
+
+            $bot->sendMessage($message->getChat()->getId(), $answer);
+
+        } else {
+            $user = $message->getFrom();
+            $answer = 'Спасибо большое ' . $user->getFirstName() . '. Еще раз спасибо. Отдуши!';
+            $bot->sendMessage($message->getChat()->getId(), $answer);
+            $bot->sendMessage("@hinrybday", "Поздравление от ".$user->getFirstName()." - ".$text);
+        }
+    });*/
+    $bot->on(function ($Update) use ($bot) {
+        $message = $Update->getMessage();
+        $msg_text = $message->getText();
+
+        $bot->sendMessage($message->getChat()->getId(), "Я не понимаю ...");
+    }, function () {
+        return true;
+    });
+
+    $bot->run();
+
+} catch (\TelegramBot\Api\Exception $e) {
+    $e->getMessage();
+}
 if(isset($_POST['first_name'])){
     $firstName = $_POST['first_name'];
     $userCity = $_POST['user_city'];
@@ -22,48 +66,5 @@ if(isset($_POST['first_name'])){
         echo false;
     }
 
-    try {
-        $bot = new \TelegramBot\Api\Client($token);
 
-        $bot->command('start', function ($message) use ($bot) {
-            $answer = 'Привет! Я буду отправлять тебе сообщения с сайта твой чат айди - '.$message->getChat()->getId();
-            $bot->sendMessage($message->getChat()->getId(), $answer);
-        });
-
-// команда для помощи
-        $bot->command('help', function ($message) use ($bot) {
-            $answer = 'Команды:
-        /help - вывод справки';
-            $bot->sendMessage($message->getChat()->getId(), $answer);
-        });
-        /*$bot->command('bday', function ($message) use ($bot) {
-            $text = $message->getText();
-            $param = str_replace('/bday', '', $text);
-            if (empty($param)) {
-                $answer = 'Вы ничего не написали. Что бы отправить поздравление в начале текста введите команду /bday затем текст.
-            Пример "/bday с др"';
-
-                $bot->sendMessage($message->getChat()->getId(), $answer);
-
-            } else {
-                $user = $message->getFrom();
-                $answer = 'Спасибо большое ' . $user->getFirstName() . '. Еще раз спасибо. Отдуши!';
-                $bot->sendMessage($message->getChat()->getId(), $answer);
-                $bot->sendMessage("@hinrybday", "Поздравление от ".$user->getFirstName()." - ".$text);
-            }
-        });*/
-        $bot->on(function ($Update) use ($bot) {
-            $message = $Update->getMessage();
-            $msg_text = $message->getText();
-
-            $bot->sendMessage($message->getChat()->getId(), "Я не понимаю ...");
-        }, function () {
-            return true;
-        });
-
-        $bot->run();
-
-    } catch (\TelegramBot\Api\Exception $e) {
-        $e->getMessage();
-    }
 }
