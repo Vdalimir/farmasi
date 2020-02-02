8ea6092fb287adde55253e6384fc4902c1d84e17
Vdalimir <vladimir.hrinin@gmail.com>
2020-02-02 21:59:15 +0200

    add telegram bot

--- gadsform.php
+++ gadsform.php
@@ -8,7 +8,7 @@ if(isset($_POST)){
     $data = json_decode(file_get_contents('php://input'));
 
     if($gkey == $data->google_key){
-        $text = "Заявка с гугл формы - ". $data->user_column_data;
+        $text = "Заявка с гугл формы - ". $data->user_column_data->string_value;
         $bot->sendMEssage(450790032, $text);
     }
 
