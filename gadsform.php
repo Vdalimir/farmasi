07dc8c2f482dba74f69103c78870ed477aabd3fb
Vdalimir <vladimir.hrinin@gmail.com>
2020-02-02 21:28:34 +0200

    add telegram bot

--- gadsform.php
+++ gadsform.php
@@ -3,10 +3,9 @@
 include "sendmail.php";
 require_once "vendor/autoload.php";
 
-
 if(isset($_POST)){
     $data = json_decode(file_get_contents('php://input'), true);
 
 
-    $bot->sendMEssage(450790032, "Тест гугл -ключ --".$data->google_key);
+    $bot->sendMEssage(450790032, "Тест гугл -ключ --".$data->{'google-key'});
 }
